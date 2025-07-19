f34b0a46a5d295aab713a4b06b4fd43402115505
Daniel Redelius <69859445+McTeaCup@users.noreply.github.com>
2025-07-20 00:14:35 +0200

    remade whole action

--- .github/actions/pr_react/react_to_pr.js
+++ .github/actions/pr_react/react_to_pr.js
@@ -1,25 +1,53 @@
-//Requierd dependencies
-const github = require('@actions/github')
 const core = require('@actions/core')
+const github = require('@actions/github')
 
-async function run() {
-    const myToken = core.getInput('myToken')
-    const bot = github.getOctokit(myToken)
+try {
+    const owner = github.context.owner
+    const repo = github.context.repo
 
-    const context = github.context;
-    const author = context.actor
+    //Get input values
+    const pr_num = core.getInput(`pr_number`, {required: true})
+    const token = core.getInput(`token`, {required: true})
 
-    let currentDate = new Date().toJSON().slice(0, 10);
-    let currentTime = new Date().toJSON().slice(11, 16);
+    //Get octokit agent
+    const octokit = new github.getOctokit(token)
 
-    const time = `${currentDate} (${currentTime})`
+    //Get files changed in PR
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+        owner,
+        repo,
+        pull_number: pr_num,
+    })
 
-    bot.rest.issues.create({
-        owner: context.owner,
-        repo: context.repo,
-        title: `Test PR issue`,
-        body: `A new pull request has been created by ${author.toString()} at ${time}`
+    //Get diff data
+    let diffData = {
+        additions: 0,
+        deletions: 0,
+        changes: 0,
+    }
+
+    //Summarize all diff data and set data to "diffData"
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions;
+      acc.deletions += file.deletions;
+      acc.changes += file.changes;
+      return acc;
+    }, diffData);
+    
+    //Create the issue
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+        Pull Request #${pr_number} has been updated with: \n
+        - ${diffData.changes} changes \n
+        - ${diffData.additions} additions \n
+        - ${diffData.deletions} deletions \n
+      `
     });
+
+} catch (error){
+    core.setFailed(error.message)
 }
 
-run();
